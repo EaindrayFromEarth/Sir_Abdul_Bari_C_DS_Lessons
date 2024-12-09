c3a237285d363c7a7db8e24a56c4be8cd2b358c0
Eaindray <eaindrayfromearth@gmail.com>
2024-12-09 11:09:56 +0700

    Variable-Sized array

--- Introduction/array_basic.cpp
+++ Introduction/array_basic.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main()
 {
-    int B[5]= {2,4,6,8,20};
-//declaration  inittailization
+    int B[10]= {2,4,6,8,20};
+//declaration and initialization
     int A[5];
     A[0]=12;
     A[1]=20;
@@ -14,12 +14,20 @@ int main()
     cout<<sizeof(A)<<endl;    //20
     cout<<A[1]<<endl;         //15
     printf("%d\n", A[2]);     //25
-    
+
     printf("%d\n", A[6]);     //0
     // In online GDB compiler, Accessing A[6] is out of bounds and leads to undefined behavior.
     // It may return a garbage value or cause a crash. (-1303713792)
+    for(int i=0; i<10;i++)
+    {
+        cout<<B[i]<<endl;
+    }                //2 4 6 8 20 0 0 0 0 0
 
- 
+    for(int x:A)
+    {
+        cout<<x<<endl;
+    }                //12 20 25 0 0
+
     return 0;
 } // namespace std
 
