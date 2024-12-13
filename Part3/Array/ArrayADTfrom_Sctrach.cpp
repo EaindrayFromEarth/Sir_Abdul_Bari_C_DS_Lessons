fe8e5bb2706caea1eb03beb6fd63c2475984cb03
Eaindray <eaindrayfromearth@gmail.com>
2024-12-13 13:57:30 +0700

    InsertSort, Checked Sorted, Rearrange, Merge, Union, Intersection,Difference,

--- Part3/Array/ArrayADTfrom_Sctrach.cpp
+++ Part3/Array/ArrayADTfrom_Sctrach.cpp
@@ -166,6 +166,158 @@ void Reverse2(struct Array *arr)
     }
 }
 
+void InsertSort(struct Array *arr, int x)
+{
+    int i = arr->length - 1;
+    if (arr->length == arr->size)
+        return;
+    while (i >= 0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+int isSorted(struct Array arr)
+{
+    int i;
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+            return 0;
+    }
+    return 1;
+}
+void Rearrange(struct Array *arr)
+{
+    int i, j;
+    i = 0;
+    j = arr->length - 1;
+
+    while (i < j)
+    {
+        while (arr->A[i] < 0)
+            i++;
+        while (arr->A[j] >= 0)
+            j--;
+        if (i < j)
+            swap(&arr->A[i], &arr->A[j]);
+    }
+}
+struct Array *Merge(struct Array *arr1, struct Array
+                                            *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array
+                              *)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+
+    return arr3;
+}
+struct Array *Union(struct Array *arr1, struct Array
+                                            *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array
+                              *)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            arr3->A[k++] = arr2->A[j++];
+        else
+        {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+struct Array *Intersection(struct Array *arr1, struct
+                           Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array
+                              *)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            i++;
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else if (arr1->A[i] == arr2->A[j])
+        {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+struct Array *Difference(struct Array *arr1, struct
+                         Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array
+                              *)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main()
 {
     struct Array arr1;
