077f05d8ea0911703f42e76893df160193bab28f
Eaindray <eaindrayfromearth@gmail.com>
2024-12-27 10:12:11 +0700

    Reqursive Merge Sort

---
+++ Part4/reqursive_merge_sort.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+
+// Recursive MergeSort
+void Merge(int A[], int l, int mid, int h)
+{
+    int i = l, j = mid + 1, k = l;
+    int B[100];
+
+    while (i <= mid && j <= h)
+    {
+        if (A[i] < A[j])
+            B[k++] = A[i++];
+        else
+            B[k++] = A[j++];
+    }
+    for (; i <= mid; i++)
+        B[k++] = A[i];
+    for (; j <= h; j++)
+        B[k++] = A[j];
+
+    for (i = l; i <= h; i++)
+        A[i] = B[i];
+}
+void MergeSort(int A[], int l, int h)
+{
+    int mid;
+    if (l < h)
+    {
+        mid = (l + h) / 2;
+        MergeSort(A, l, mid);
+        MergeSort(A, mid + 1, h);
+        Merge(A, l, mid, h);
+    }
+}
+int main()
+{
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10, i;
+
+    MergeSort(A, 0, 9);
+
+    for (i = 0; i < 10; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+
+    return 0;
+}
+
+// Expected output: 3 5 7 9 10 11 12 13 16 24
